adfdb30938315686394a229215be0567acf8ac65
Sethtek Dev <sethtekdev@gmail.com>
2023-02-25 14:18:11 -0800

    Config file changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,23 +6,23 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Sethtek Sandbox',
+  tagline: 'A highly modifyable multiplayer sandbox game.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://sandbox.sethtek.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
+  organizationName: 'Sethtek', // Usually your GitHub org/user name.
+  projectName: 'Sethtek-Sandbox-Website', // Usually your repo name.
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: 'deploy',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -64,10 +64,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Sethtek Sandbox',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Sethtek Sandbox Site Logo',
+          src: 'img/logo.png',
         },
         items: [
           {
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Sethtek Dev. Built with the amazing Docusaurus!`,
       },
       prism: {
         theme: lightCodeTheme,
